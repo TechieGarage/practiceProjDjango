c115c139815949f35ce733507af9a60b6c7c0f46
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-09-12 15:07:44 +0300

    modified model

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class MyUsers(models.Model):
     fname = models.CharField(max_length=20)
     lname = models.CharField(max_length=20)
-    myEmail = models.CharField(max_length=30)
+    myEmail = models.EmailField(max_length=30, unique=True)
 
     def __str__(self):
         return self.fname + " " + self.lname + " " + self.myEmail
