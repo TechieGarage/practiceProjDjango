1d343ff2fc92607cb12eaeb203d9013d752fcfc6
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-09-12 13:12:47 +0300

    firstApp with model view template created with faker data

---
+++ firstApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from firstApp.models import Topic, Webpage, AccessRecord
+
+# Create your views here.
+
+def index(request):
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    return render(request, 'firstApp/index.html', date_dict)
---
+++ populate_firstApp.py
@@ -0,0 +1,42 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstProject.settings')
+
+import django
+django.setup()
+
+# FAKE POPULATION SCRIPT
+import random
+from firstApp.models import Topic, Webpage, AccessRecord
+from faker import Faker
+
+fakegen = Faker()
+topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
+
+def add_topic():
+    t = Topic.objects.get_or_create(topic_name=random.choice(topics))[0] # [0] returns reference to
+    t.save()                                                             # model instance
+    return t
+
+def populate(N=5):
+    for entry in range(N):
+        # Get the topic for the entry
+        top = add_topic()
+
+        # Create fake data for that entry
+        fakeurl = fakegen.url()
+        fakedate = fakegen.date()
+        fakename = fakegen.company()
+
+        # Create the new webpage entry
+        webpg = Webpage.objects.get_or_create(topic=top, url=fakeurl, name=fakename)[0]
+
+        # Create a fake access record for that webpage
+        acc_rec = AccessRecord.objects.get_or_create(name=webpg, date=fakedate)[0]
+
+
+
+if __name__ == '__main__':
+    print('Populating script!')
+    populate(20)
+    print('Populating complete!')
+
