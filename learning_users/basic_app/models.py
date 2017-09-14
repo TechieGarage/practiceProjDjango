beda59cd5a1aba1abcd5b0574aa5b2615e18b333
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-09-14 12:37:49 +0300

    User model and Form

--- learning_users/basic_app/models.py
+++ learning_users/basic_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User) # here we are not inheriting User model as it may lead to creation
+                             # of multiple instances in database. so we simply create one-to-one relation
+    # Additional field adding into user
+    portfolio_site = models.URLField(blank=True) # Optional
+    profile_pic = models.ImageField(upload_to='profile_pics' , blank=True) # profile_pics is subfolder of
+                                                                           # media folder
+    def __str__(self):
+        return self.user.username
