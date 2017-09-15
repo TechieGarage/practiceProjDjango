f108d9d21255e2e5be3a3f0888d1a5ec7701ea5a
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-09-15 08:25:23 +0530

    class based view initiated

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from basic_app import views
+from django.views.generic import View
+from django.http import HttpResponse
+
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+#def index(request):
+#    return render(request, 'index.html')
+
+class CBView(View):
+    def get(self, request):
+        return HttpResponse("Hello World!")
