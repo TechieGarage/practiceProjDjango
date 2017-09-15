d6ae76a9d461e7b9c7f063996a136ffa45896543
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-09-15 08:42:03 +0530

    Template view

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import View, TemplateView
+
 
 # Create your views here.
 #def index(request):
 #    return render(request, 'index.html')
 
-class CBView(View):
-    def get(self, request):
-        return HttpResponse("Hello World!")
+#class CBView(View):
+#    def get(self, request):
+#        return HttpResponse("Hello World!")
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)  # creating context dictionary
+        context['injectme'] = "basic injection"     # now storing key-value pair in dictionary
+        return context
